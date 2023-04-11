b9e0777880e8426e4d9ca7084979d21f4bb511c3
curran vanwaarde <curranv@callscaler.com>
2023-04-11 11:38:09 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [sessions, setSessions] = useState([]);
+
+    useEffect(() => {
+        fetchSessions();
+    }, []);
+
+    const fetchSessions = async () => {
+        const response = await axios.get('https://rocky-hollows-13643.herokuapp.com/api/sessions');
+        setSessions(response.data);
+    };
+
+    const copySnippet = () => {
+        const snippet = document.getElementById('snippet');
+        snippet.select();
+        document.execCommand('copy');
+    };
+
+    return (
+        <div>
+            <h1>Simple Website Traffic Tracker</h1>
+            <button onClick={copySnippet}>Copy JavaScript Snippet</button>
+            <textarea id="snippet" readOnly value="<script src='https://rocky-hollows-13643.herokuapp.com/tracker.js'></script>" />
+            <h2>Visitor Sessions</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Session ID</th>
+                        <th>User Agent</th>
+                        <th>Platform</th>
+                        <th>Timestamp</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {sessions.map(session => (
+                        <tr key={session._id}>
+                            <td>{session.sessionId}</td>
+                            <td>{session.userAgent}</td>
+                            <td>{session.platform}</td>
+                            <td>{new Date(session.createdAt).toLocaleString()}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
